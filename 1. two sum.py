f3b2957d34f317b8d6ba1a746ed2c1e1a2c154e3
Dewarshi Shukla <86153335+Dewrshi@users.noreply.github.com>
2023-03-28 01:12:25 +0530

    Update 1. two sum.py

--- 1. two sum.py
+++ 1. two sum.py
@@ -1,6 +1,3 @@
-Python Solution :-------------------
-
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
     #brute force will cause error due to time complexity
@@ -16,21 +13,3 @@ class Solution:
                 values[value] = idx
 
 
-
-Java Solution :--------------------
-	
-	class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer,Integer> map = new HashMap<Integer,Integer>();
-        int complement;
-
-        for(int i=0;i<nums.length;i++){
-            complement = target - nums[i];
-            if(map.containsKey(complement)){
-                return new int[] {i,map.get(complement)}; 
-            }
-            map.put(nums[i],i);
-        }
-        throw new IllegalArgumentException("No solution");     
-    }
-}
